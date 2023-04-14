64b78f5cc4333cab188e34828e6fb081c814b579
hill <hillonyechekwa@gmail.com>
2023-04-14 01:21:12 +0100

    made updates to functions

--- netlify/functions/payments/payments.js
+++ netlify/functions/payments/payments.js
@@ -9,7 +9,7 @@ const handler = async (event) => {
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
     }
-  } catch (error) {P
+  } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
